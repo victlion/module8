fc78c830d54eeb4efa4b25f5de408dcf46fbd35a
victlion <143883849+victlion@users.noreply.github.com>
2023-09-03 13:49:08 +0300

    Update ShapePrinter.java

--- src/main/java/org/example/core/ShapePrinter.java
+++ src/main/java/org/example/core/ShapePrinter.java
@@ -1,6 +1,5 @@
 package org.example.core;
 
-// Класс для вывода имени фигуры
 public class ShapePrinter {
     public void printShapeName(Shape shape) {
         System.out.println("Shape name: " + shape.getName());
